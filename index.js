e2ac8bca88560359b54109b2ec407b962c48d329
Qzarov <qzarov@mail.ru>
2023-07-04 03:03:28 +0300

    Add a simple server who gets messages from parsers

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
-const cors = require('cors');
+
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
 dotenv.config()
 
 const app = express();
-app.use(express.json());
-app.use(cors())
+app.use(express.static('localhost'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
 
 const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
+const jsonParser = bodyParser.json();
 
 bot.on('message', async (msg) => {
     console.log("Start routing")
@@ -40,4 +45,44 @@ bot.on('message', async (msg) => {
     bot.sendMessage(json_msg.target, text, {
         reply_markup: {}
     }).then();
+})
+
+app.get('/', (request, response) => {
+    console.log(`URL: ${request.url}`);
+    response.send('Parser, is it you?');
+});
+
+app.post(`/api/router/message`, jsonParser, (req, res) => {
+    // const authData = req.auth
+
+    if (!req.body) return res.sendStatus(400);
+    const body = req.body;
+
+    let messageSource = '';
+    if (body.hasOwnProperty("group")) {
+        messageSource = `From user: ${body.from}\n` +
+                        `Group: ${body.group}\n`
+    } else if (body.hasOwnProperty("channel")) {
+        messageSource = `Channel: ${body.channel}\n`
+    }
+
+    const text = `${body.text}\n\n`          +
+        messageSource +
+        `Link: ${body.link}\n`       +
+        `Keys: ${body.keywords}`;
+
+    console.log("new message:", text)
+
+    bot.sendMessage(body.target, text, {
+        reply_markup: {}
+    }).then();
+
+    res.sendStatus(200)
+})
+
+const port = 8000
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`Server listening on port ${server.address().port}\n`);
 })
