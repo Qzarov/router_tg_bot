61e4950916f81453ee822a6975748bc1eabaca1d
Yaroslav <qzarov@mail.ru>
2023-08-23 23:28:42 +0000

    Add welcome message

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config()
 
 const app = express();
-app.use(express.static('localhost'));
+app.use(express.static('164.92.143.234'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
@@ -18,33 +18,95 @@ const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {polling: true});
 const jsonParser = bodyParser.json();
 
+
+bot.on('new_chat_participant', async (msg) => {
+    console.log(`bot was adddded, get msg: ${msg}`)
+    console.log(`${msg["new_chat_participant"]}`)
+    bot_obj = await bot.get_me()
+    bot_id = bot_obj.id
+
+    for(let chat_member of msg.new_chat_members) {
+        if (chat_member.id === bot_id) {
+            console.log(`I was added!`)
+            // await message.reply(`This channel is private. You can connect it to the bot terminal via the channel ID: ${chanId}`)
+        }
+            
+    }
+})
+
+
+bot.on('my_chat_member', async (msg) => {
+    // console.log(`my_chat_member: ${JSON.stringify(msg)}`)
+
+    chat = {
+        id: msg.chat.id,
+        title: msg.chat.title,
+        username: msg.chat.username,
+        type: msg.chat.type
+    }
+
+    new_chat_member = {
+        user: {
+            id: msg.new_chat_member.user.id,
+            is_bot: true,
+        },
+        status: msg.new_chat_member.status,
+        can_post_messages: msg.new_chat_member.can_post_messages,
+    }
+    console.log(`new member status: ${new_chat_member.status}`)
+    if (new_chat_member.status !== "kicked") {
+        const text = `This channel is private. You can connect it to the bot terminal via the channel ID: \`${chat.id}\``
+        console.log(chat.id, text)
+        try {
+            await bot.sendMessage(chat.id, text, {
+                parse_mode: `Markdown`,
+                reply_markup: {}
+            }).then();
+        } catch (err) {
+            console.log(`Err: ${err}`)
+        }
+
+    }
+})
+
+
 bot.on('message', async (msg) => {
     console.log("Start routing")
-    const chat_id = msg.chat.id;
-    const json_msg = JSON.parse(msg.text);
-    const from_user = {
-        id: msg.from.id,
-        username: msg.from.username,
+    // console.log(`get msg: ${JSON.stringify(msg)}`)
+
+    // let text = ""
+    // if (msg.hasOwnProperty("text")) {
+    //     text = msg.text;
+    // }
+    // console.log(`get text: ${text}`)
+
+    let chat = {}
+    if (msg.hasOwnProperty("chat")) {
+        chat = {
+            id: msg.chat.id,
+            username: msg.chat.username,
+            title: msg.chat.title,
+        } 
+    }    
+    console.log(`get chat ${chat.title}: ${chat.id}, ${chat.username}`)
+
+    from_user = {}
+    if (msg.hasOwnProperty("from")) {
+        from_user = {
+            id: msg.from.id,
+            username: msg.from.username,
+        }
+    }
+    console.log(`get from_user: ${from_user.username}, ${from_user.id}`)
+
+    new_chat_member = {}
+    if (msg.hasOwnProperty("new_chat_member")) {
+        new_chat_member = {
+            id: msg.new_chat_member.id,
+            username: msg.new_chat_member.username,
+        }
     }
 
-    let source = "";
-    if (json_msg.hasOwnProperty("group")) {
-        source = // `From user: ${json_msg.from}\n` +
-                 `Group: ${json_msg.group}\n`
-    } else if (json_msg.hasOwnProperty("channel")) {
-        source = `Channel: ${json_msg.channel}\n`
-    }
-
-    const text = `${json_msg.text}\n\n`          +
-                 source +
-                `Link: ${json_msg.link}\n`       +
-                `Keys: ${json_msg.keywords}`;
-
-    console.log("new message:", text)
-
-    bot.sendMessage(json_msg.target, text, {
-        reply_markup: {}
-    }).then();
 })
 
 app.get('/', (request, response) => {
