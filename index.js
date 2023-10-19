5a134d42855b7de36859ec0e13ed065af9a5179e
Yaroslav <qzarov@mail.ru>
2023-10-19 21:15:14 +0000

    Add routing via different bots

--- index.js
+++ index.js
@@ -1,113 +1,28 @@
-const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
+// const TelegramBot = require('node-telegram-bot-api');
+import TelegramBot from 'node-telegram-bot-api'
+import express from 'express'
+import bodyParser from 'body-parser'
 
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+import {serverAddress, tgBotTokens, supportedPlatforms} from './config.js'
 
-dotenv.config()
 
+console.log(`Server address: ${serverAddress}`)
 const app = express();
-app.use(express.static('164.92.143.234'));
+app.use(express.static(serverAddress));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
 
-const token = process.env.BOT_TOKEN;
-
-const bot = new TelegramBot(token, {polling: true});
+//const token = process.env.BOT_TOKEN;
+// const bot = new TelegramBot(token, {polling: true});
 const jsonParser = bodyParser.json();
 
-
-bot.on('new_chat_participant', async (msg) => {
-    console.log(`bot was adddded, get msg: ${msg}`)
-    console.log(`${msg["new_chat_participant"]}`)
-    bot_obj = await bot.get_me()
-    bot_id = bot_obj.id
-
-    for(let chat_member of msg.new_chat_members) {
-        if (chat_member.id === bot_id) {
-            console.log(`I was added!`)
-            // await message.reply(`This channel is private. You can connect it to the bot terminal via the channel ID: ${chanId}`)
-        }
-            
-    }
-})
-
-
-bot.on('my_chat_member', async (msg) => {
-    // console.log(`my_chat_member: ${JSON.stringify(msg)}`)
-
-    chat = {
-        id: msg.chat.id,
-        title: msg.chat.title,
-        username: msg.chat.username,
-        type: msg.chat.type
-    }
-
-    new_chat_member = {
-        user: {
-            id: msg.new_chat_member.user.id,
-            is_bot: true,
-        },
-        status: msg.new_chat_member.status,
-        can_post_messages: msg.new_chat_member.can_post_messages,
-    }
-    console.log(`new member status: ${new_chat_member.status}`)
-    if (new_chat_member.status !== "kicked") {
-        const text = `This channel is private. You can connect it to the bot terminal via the channel ID: \`${chat.id}\``
-        console.log(chat.id, text)
-        try {
-            await bot.sendMessage(chat.id, text, {
-                parse_mode: `Markdown`,
-                reply_markup: {}
-            }).then();
-        } catch (err) {
-            console.log(`Err: ${err}`)
-        }
-
-    }
-})
-
-
-bot.on('message', async (msg) => {
-    console.log("Start routing")
-    // console.log(`get msg: ${JSON.stringify(msg)}`)
-
-    // let text = ""
-    // if (msg.hasOwnProperty("text")) {
-    //     text = msg.text;
-    // }
-    // console.log(`get text: ${text}`)
-
-    let chat = {}
-    if (msg.hasOwnProperty("chat")) {
-        chat = {
-            id: msg.chat.id,
-            username: msg.chat.username,
-            title: msg.chat.title,
-        } 
-    }    
-    console.log(`get chat ${chat.title}: ${chat.id}, ${chat.username}`)
-
-    from_user = {}
-    if (msg.hasOwnProperty("from")) {
-        from_user = {
-            id: msg.from.id,
-            username: msg.from.username,
-        }
-    }
-    console.log(`get from_user: ${from_user.username}, ${from_user.id}`)
-
-    new_chat_member = {}
-    if (msg.hasOwnProperty("new_chat_member")) {
-        new_chat_member = {
-            id: msg.new_chat_member.id,
-            username: msg.new_chat_member.username,
-        }
-    }
-
-})
+let bots = {}
+for (const [ name, token ] of Object.entries(tgBotTokens)) {
+    console.log(`adding bot ${name} with token ${token}`)
+    bots[name] = new TelegramBot(token, {polling: false});
+}
 
 app.get('/', (request, response) => {
     console.log(`URL: ${request.url}`);
@@ -120,6 +35,32 @@ app.post(`/api/router/message`, jsonParser, (req, res) => {
 
     if (!req.body) return res.sendStatus(400);
     const body = req.body;
+
+    let platform = "telegram";
+    if (body.hasOwnProperty("platform")) {
+        try {
+            if (supportedPlatforms.indexOf(body.platform) === -1) {
+                throw error(`⛔️ Not supported platform in message body: ${body.platform}`)
+            }
+            platform = body.platform;
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    let botName = "TrueRouterBot";
+    if (body.hasOwnProperty("botName")) {
+        const botNamesList = Object.keys(tgBotTokens)
+        try {
+            if (botNamesList.indexOf(body.botName) === -1) {
+                throw error(`⚠️ Unknown botName in message body: ${body.botName}`)
+            }
+            botName = body.botName;
+        } catch (err) {
+            console.log(err)
+        }
+        botName = body.botName;
+    }
 
     let message = body.text + '\n\n';
 
@@ -140,7 +81,11 @@ app.post(`/api/router/message`, jsonParser, (req, res) => {
 
     console.log("new message:", message)
     try {
-        bot.sendMessage(body.target, message, {
+        if (!body.hasOwnProperty("target")) {
+            throw err(`⛔️ Property "target" is not provided`)
+        }
+
+        bots[botName].sendMessage(body.target, message, {
             // parse_mode: `Markdown`,
             reply_markup: {}
         }).then();
@@ -150,9 +95,6 @@ app.post(`/api/router/message`, jsonParser, (req, res) => {
     } catch (err) {
         console.log(`Error occured: ${err}`)
     }
-
-
-    
 })
 
 const port = 8000
